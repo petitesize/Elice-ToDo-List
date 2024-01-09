0c8e57ff950674512f5d534001938683e93c8e2d
petitesize <petitesize22@gmail.com>
2024-01-09 14:50:55 +0900

    검색 기능

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".memos");
 const alertmsg = document.querySelector("#alertmsg");
+const search = document.querySelector(".search input");
 
 const saveMemo = (memotext) => {
   const html = `<li
@@ -24,4 +25,34 @@ addForm.addEventListener("submit", (e) => {
   } else {
     alertmsg.classList.remove("hidden");
   }
+});
+
+// 메모 삭제
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+const filterMemo = (memo) => {
+  Array.from(list.children).forEach((memotext) => {
+    const hasMemo = memotext.textContent.includes(memo);
+    memotext.classList.toggle("filtered", !hasMemo);
+  });
+  //   Array.from(list.children)
+  //     // 검색어가 포함되지 않은 것
+  //     .filter((memotext) => !memotext.textContent.includes(memo))
+  //     // 을 지워준다
+  //     .forEach((memotext) => memotext.classList.add("filtered"));
+
+  //   Array.from(list.children)
+  //     .filter((memotext) => memotext.textContent.includes(memo))
+  //     .forEach((memotext) => memotext.classList.remove("filtered"));
+};
+
+// 메모 검색
+search.addEventListener("keyup", (e) => {
+  const searchText = search.value;
+  list.innerHTML;
+  filterMemo(e.target.value);
 });
